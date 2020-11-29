eb535579c792d1c1cc891818ea4310054beeff24
YerzhAlmash <74873242+YerzhAlmash@users.noreply.github.com>
2020-11-29 21:12:14 +0600

    shop

---
+++ шоп/dbo.Book.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Book] (
+    [Id]          INT            NOT NULL IDENTITY,
+    [name]        NVARCHAR (MAX) NULL,
+    [author]      NVARCHAR (MAX) NULL,
+    [Janr]        NVARCHAR (MAX) NULL,
+    [Otzyvy]      NVARCHAR (MAX) NULL,
+    [img]         NVARCHAR (MAX) NULL,
+    [price]       INT            NULL,
+    [isFavourite] BIT            NULL,
+    [ available ] BIT            NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
